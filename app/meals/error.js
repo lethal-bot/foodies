9141aee71615b9aa033ca5ed127b41bd16cf76a7
Aditya Keshari <aditya165.in@gmail.com>
2024-06-26 01:59:50 +0530

    frontend of foodies

---
+++ app/meals/error.js
@@ -0,0 +1,7 @@
+'use client'
+export default function Error({ error }) {
+    return <main className="error">
+        <h1>An error occured</h1>
+        <p>failed to fetch meal data. plz try again later</p>
+    </main>
+}
